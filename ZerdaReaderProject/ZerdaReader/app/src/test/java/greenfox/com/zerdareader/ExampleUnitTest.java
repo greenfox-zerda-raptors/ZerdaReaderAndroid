6008d9cb6910da1b79fab8569ff9f0cb7c16ccb0
regnisalram <reka.bence@gmail.com>
2017-01-18 12:08:52 +0100

    button added, changes textview

--- ZerdaReaderProject/ZerdaReader/app/src/test/java/greenfox/com/zerdareader/ExampleUnitTest.java
+++ ZerdaReaderProject/ZerdaReader/app/src/test/java/greenfox/com/zerdareader/ExampleUnitTest.java
@@ -1,6 +1,9 @@
 package greenfox.com.zerdareader;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
 
 import static org.junit.Assert.*;
 
@@ -9,7 +12,10 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class)
 public class ExampleUnitTest {
+
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
