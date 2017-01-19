8d949a66e251ec9d2b718a77725b8679e7764474
vikukzs <vikuk.zs@gmail.com>
2017-01-19 16:54:50 +0100

    add @SerializedName annotations

--- app/src/main/java/com/greenfox/fuchsit/zerdareader/model/User.java
+++ app/src/main/java/com/greenfox/fuchsit/zerdareader/model/User.java
@@ -1,11 +1,16 @@
 package com.greenfox.fuchsit.zerdareader.model;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by Anna on 17/01/16.
  */
 
 public class User {
+
     private int id;
+    @SerializedName("email")
     private String email;
+    @SerializedName("password")
     private String password;
 }
--- app/src/main/java/com/greenfox/fuchsit/zerdareader/model/UserResponse.java
+++ app/src/main/java/com/greenfox/fuchsit/zerdareader/model/UserResponse.java
@@ -1,13 +1,18 @@
 package com.greenfox.fuchsit.zerdareader.model;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by Zsuzska on 2017. 01. 19..
  */
 
 public class UserResponse {
 
+    @SerializedName("id")
     private long id;
+    @SerializedName("result")
     private boolean success;
+    @SerializedName("token")
     private String token;
 
 }
