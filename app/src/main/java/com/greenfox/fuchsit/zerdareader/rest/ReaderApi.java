61e781c14e2893efc9866e8eece3c5431ab557b8
vikukzs <vikuk.zs@gmail.com>
2017-01-19 16:59:33 +0100

    add ReaderApi class

---
+++ app/src/main/java/com/greenfox/fuchsit/zerdareader/rest/ReaderApi.java
@@ -0,0 +1,24 @@
+package com.greenfox.fuchsit.zerdareader.rest;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by Zsuzska on 2017. 01. 19..
+ */
+
+public class ReaderApi {
+
+    public static final String BASE_URL = "";
+    private static Retrofit retrofit = null;
+
+    public static Retrofit getClient() {
+        if (retrofit==null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
