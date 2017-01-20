c48de59be21e44f01e7573b77dbf59135c380ba5
zsofinnemeth <zsofinnemeth@gmail.com>
2017-01-20 12:09:10 +0100

    loginactivity with shared preferences

--- app/src/main/java/com/greenfox/fuchsit/zerdareader/activity/LoginActivity.java
+++ app/src/main/java/com/greenfox/fuchsit/zerdareader/activity/LoginActivity.java
@@ -1,11 +1,14 @@
 package com.greenfox.fuchsit.zerdareader.activity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.greenfox.fuchsit.zerdareader.R;
 
@@ -14,6 +17,7 @@ public class LoginActivity extends AppCompatActivity {
     Button button;
     EditText editUserName, editPassword;
     TextView textView;
+    TextView dataView;
 
 
     @Override
@@ -22,6 +26,7 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.login);
 
         textView = (TextView) findViewById(R.id.loginTitle);
+        dataView = (TextView) findViewById(R.id.dataTextView);
 
         editUserName = (EditText) findViewById(R.id.userName);
         editPassword = (EditText) findViewById(R.id.password);
@@ -34,5 +39,24 @@ public class LoginActivity extends AppCompatActivity {
                 textView.setText("Login Button works!");
             }
         });
+    }
+
+    //Save login info
+    public void saveData(View view){
+        SharedPreferences loginData = getSharedPreferences("userInfo", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = loginData.edit();
+        editor.putString("userName", editUserName.getText().toString());
+        editor.putString("password", editPassword.getText().toString());
+        editor.apply();
+
+        Toast.makeText(this,"Saved",Toast.LENGTH_LONG).show();
+    }
+
+    public void getData(View view){
+        SharedPreferences loginData = getSharedPreferences("userInfo", Context.MODE_PRIVATE);
+        String name = loginData.getString("userName", "");
+        String pw = loginData.getString("password","");
+        String msg = "Saved User Name: " + name + "\nSaved Password: " + pw;
+        dataView.setText(msg);
     }
 }
