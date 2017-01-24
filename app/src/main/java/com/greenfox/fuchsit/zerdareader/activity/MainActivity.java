377ecd05e11daab2db4d20192e36ab7b75a69fd1
zsofinnemeth <zsofinnemeth@gmail.com>
2017-01-24 12:15:52 +0100

    Merge branch 'zsofi-reka-shared-pref' of https://github.com/greenfox-zerda-raptors/ZerdaReaderAndroid into zsofi-reka-shared-pref
    # Conflicts:
    #	app/src/main/java/com/greenfox/fuchsit/zerdareader/activity/LoginActivity.java
    #	app/src/main/java/com/greenfox/fuchsit/zerdareader/activity/MainActivity.java
    #	app/src/main/res/layout/activity_main.xml
    #	app/src/main/res/layout/login.xml

--- app/src/main/java/com/greenfox/fuchsit/zerdareader/activity/MainActivity.java
+++ app/src/main/java/com/greenfox/fuchsit/zerdareader/activity/MainActivity.java
@@ -2,9 +2,12 @@ package com.greenfox.fuchsit.zerdareader.activity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import com.greenfox.fuchsit.zerdareader.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
         sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
         checkIfLoggedIn();
     }
