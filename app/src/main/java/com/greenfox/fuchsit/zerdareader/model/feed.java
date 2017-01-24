6bf10454fa043d2e1764c721a7476d0fcf388cc2
vikukzs <vikuk.zs@gmail.com>
2017-01-24 11:42:11 +0100

    refactor listItem and feed classnames

--- app/src/main/java/com/greenfox/fuchsit/zerdareader/model/feed.java
+++ app/src/main/java/com/greenfox/fuchsit/zerdareader/model/feed.java
@@ -4,7 +4,7 @@ package com.greenfox.fuchsit.zerdareader.model;
  * Created by Zsuzska on 2017. 01. 24..
  */
 
-public class feed {
+public class Feed {
     private long id;
     private String url;
 }
