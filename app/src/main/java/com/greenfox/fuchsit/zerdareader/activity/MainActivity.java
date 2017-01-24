f5531ef542aa529b02228f89456565ae3b7050b8
vikukzs <vikuk.zs@gmail.com>
2017-01-24 17:00:16 +0100

    add showNewsItems method n main activity

--- app/src/main/java/com/greenfox/fuchsit/zerdareader/activity/MainActivity.java
+++ app/src/main/java/com/greenfox/fuchsit/zerdareader/activity/MainActivity.java
@@ -5,10 +5,40 @@ import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListAdapter;
+
+import com.greenfox.fuchsit.zerdareader.adapter.FeedAdapter;
+import com.greenfox.fuchsit.zerdareader.model.NewsItem;
+import com.greenfox.fuchsit.zerdareader.rest.ReaderApiInterface;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
 
 public class MainActivity extends AppCompatActivity {
 
     SharedPreferences sharedPreferences;
+    ReaderApiInterface readerApiInterface;
+    Retrofit retrofit;
+    FeedAdapter adapter;
+
+    public void showNewsItems() {
+        readerApiInterface = retrofit.create(ReaderApiInterface.class);
+        readerApiInterface.getNewsItems().enqueue(new Callback<ArrayList<NewsItem>>() {
+            @Override
+            public void onResponse(Call<ArrayList<NewsItem>> call, Response<ArrayList<NewsItem>> response) {
+                adapter.addAll(response.body());
+            }
+
+            @Override
+            public void onFailure(Call<ArrayList<NewsItem>> call, Throwable t) {
+
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
