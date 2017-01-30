f8c425442e8c9aee976277e5663240d3fa017da0
vikukzs <vikuk.zs@gmail.com>
2017-01-30 16:05:10 +0100

    add pager adapter

---
+++ app/src/main/java/com/greenfox/fuchsit/zerdareader/adapter/PagerAdapter.java
@@ -0,0 +1,41 @@
+package com.greenfox.fuchsit.zerdareader.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import com.greenfox.fuchsit.zerdareader.activity.TabFragment1;
+import com.greenfox.fuchsit.zerdareader.activity.TabFragment2;
+
+/**
+ * Created by Zsuzska on 2017. 01. 30..
+ */
+
+public class PagerAdapter extends FragmentStatePagerAdapter {
+    int mNumOfTabs;
+
+    public PagerAdapter(FragmentManager fm, int NumOfTabs) {
+        super(fm);
+        this.mNumOfTabs = NumOfTabs;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position) {
+            case 0:
+                TabFragment1 tab1 = new TabFragment1();
+                return tab1;
+            case 1:
+                TabFragment2 tab2 = new TabFragment2();
+                return tab2;
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return mNumOfTabs;
+    }
+}
