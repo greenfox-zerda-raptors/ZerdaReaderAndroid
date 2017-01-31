cebbebddc0a15ca2a3504adeaebc36fee3636019
Anna812 <88menyus@gmail.com>
2017-01-31 10:49:12 +0100

    add ListFragmant  to the Component

--- app/src/main/java/com/greenfox/fuchsit/zerdareader/dagger/MockServerComponent.java
+++ app/src/main/java/com/greenfox/fuchsit/zerdareader/dagger/MockServerComponent.java
@@ -1,7 +1,8 @@
 package com.greenfox.fuchsit.zerdareader.dagger;
 
+import android.app.ListFragment;
+
 import com.greenfox.fuchsit.zerdareader.activity.LoginActivity;
-import com.greenfox.fuchsit.zerdareader.server.MockServer;
 
 import javax.inject.Singleton;
 
@@ -14,4 +15,5 @@ import dagger.Component;
 @Component(modules = {ReaderApiProvider.class})
 public interface MockServerComponent {
     void inject (LoginActivity loginActivity);
+    void inject (ListFragment feedFragment);
 }
