3415bddefcaddd2e87e0dd3af92723cae4ec23bf
Anna812 <88menyus@gmail.com>
2017-01-31 16:26:43 +0100

    copy Feedfragment except for calling getFavoriteNewsItems on the apiService

--- app/src/main/java/com/greenfox/fuchsit/zerdareader/activity/TabFragment2.java
+++ app/src/main/java/com/greenfox/fuchsit/zerdareader/activity/TabFragment2.java
@@ -5,8 +5,21 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.greenfox.fuchsit.zerdareader.R;
+import com.greenfox.fuchsit.zerdareader.adapter.FeedAdapter;
+import com.greenfox.fuchsit.zerdareader.dagger.DaggerMockServerComponent;
+import com.greenfox.fuchsit.zerdareader.model.NewsItem;
+import com.greenfox.fuchsit.zerdareader.rest.ReaderApiInterface;
+
+import java.util.ArrayList;
+
+import javax.inject.Inject;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * Created by Zsuzska on 2017. 01. 30..
@@ -14,9 +27,43 @@ import com.greenfox.fuchsit.zerdareader.R;
 
 public class TabFragment2 extends Fragment {
 
+    ListView feed;
+    FeedAdapter adapter;
+
+    @Inject
+    ReaderApiInterface apiService;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.tab_fragment_2, container, false);
+        View view = inflater.inflate(R.layout.tab_fragment_2, container, false);
+
+        feed = (ListView) view.findViewById(android.R.id.list);
+
+        adapter = new FeedAdapter(getActivity());
+        feed.setAdapter(adapter);
+
+        DaggerMockServerComponent.builder().build().inject(this);
+
+        showFavouriteNewsItems();
+
+        return view;
+    }
+
+    private void showFavouriteNewsItems() {
+
+        Call<ArrayList<NewsItem>> call = apiService.getFavouriteNewsItems();
+
+        call.enqueue(new Callback<ArrayList<NewsItem>>() {
+            @Override
+            public void onResponse(Call<ArrayList<NewsItem>> call, Response<ArrayList<NewsItem>> response) {
+                adapter.addAll(response.body());
+            }
+
+            @Override
+            public void onFailure(Call<ArrayList<NewsItem>> call, Throwable t) {
+
+            }
+        });
     }
 }
 
