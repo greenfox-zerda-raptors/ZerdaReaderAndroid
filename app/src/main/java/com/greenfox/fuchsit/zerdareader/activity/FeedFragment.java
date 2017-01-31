77e277b2e279e866d5498ea5875e99c73d6e067d
Anna812 <88menyus@gmail.com>
2017-01-31 10:48:17 +0100

    inject ReaderApiInterface field and dagger builder

--- app/src/main/java/com/greenfox/fuchsit/zerdareader/activity/FeedFragment.java
+++ app/src/main/java/com/greenfox/fuchsit/zerdareader/activity/FeedFragment.java
@@ -12,10 +12,14 @@ import android.widget.ListView;
 
 import com.greenfox.fuchsit.zerdareader.R;
 import com.greenfox.fuchsit.zerdareader.adapter.FeedAdapter;
+import com.greenfox.fuchsit.zerdareader.dagger.DaggerMockServerComponent;
 import com.greenfox.fuchsit.zerdareader.model.NewsItem;
+import com.greenfox.fuchsit.zerdareader.rest.ReaderApiInterface;
 import com.greenfox.fuchsit.zerdareader.server.MockServer;
 
 import java.util.ArrayList;
+
+import javax.inject.Inject;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -30,6 +34,9 @@ public class FeedFragment extends ListFragment {
     ListView feed;
     FeedAdapter adapter;
 
+    @Inject
+    ReaderApiInterface apiService;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -40,6 +47,8 @@ public class FeedFragment extends ListFragment {
         adapter = new FeedAdapter(getActivity());
         feed.setAdapter(adapter);
 
+        DaggerMockServerComponent.builder().build().inject(this);
+
         showNewsItems();
 
         return view;
@@ -49,7 +58,6 @@ public class FeedFragment extends ListFragment {
 
         //this is commented out because we will switch to this line when we'll use the real server
 //        final ReaderApiInterface apiService = api.getClient().create(ReaderApiInterface.class);
-        MockServer apiService = new MockServer();
         Call<ArrayList<NewsItem>> call = apiService.getNewsItems();
 
         call.enqueue(new Callback<ArrayList<NewsItem>>() {
