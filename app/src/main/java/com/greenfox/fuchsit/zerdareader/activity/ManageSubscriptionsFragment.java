e3a3239e833879e077b32a1f2dddaf66c33c614d
zsofinnemeth <zsofinnemeth@gmail.com>
2017-01-31 13:41:44 +0100

    manage subscriptions fragment + settings  fragment

---
+++ app/src/main/java/com/greenfox/fuchsit/zerdareader/activity/ManageSubscriptionsFragment.java
@@ -0,0 +1,10 @@
+package com.greenfox.fuchsit.zerdareader.activity;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by false on 2017. 01. 31..
+ */
+
+public class ManageSubscriptionsFragment extends Fragment {
+}
