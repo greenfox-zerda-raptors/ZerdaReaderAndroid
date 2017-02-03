2c1840477935f034e25256b91c7996adea84807e
zsofinnemeth <zsofinnemeth@gmail.com>
2017-02-03 11:48:38 +0100

    replacing toolbar icons, trying to reposition the back button, toolbar added to detailed activity

--- app/src/main/java/com/greenfox/fuchsit/zerdareader/activity/DetailedPageActivity.java
+++ app/src/main/java/com/greenfox/fuchsit/zerdareader/activity/DetailedPageActivity.java
@@ -3,7 +3,12 @@ package com.greenfox.fuchsit.zerdareader.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.greenfox.fuchsit.zerdareader.R;
 import com.greenfox.fuchsit.zerdareader.model.NewsItem;
@@ -20,10 +25,35 @@ public class DetailedPageActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.detailed_page);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
         
         NewsItem newsItem = (NewsItem) getIntent().getSerializableExtra("newsItem");
 
         article = (TextView) findViewById(R.id.description);
         article.setText(newsItem.getDescription());
     }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.my_toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.back:
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+            case R.id.favorite:
+                Toast.makeText(this, "You must be my lucky star", Toast.LENGTH_LONG).show();
+                break;
+        }
+        return true;
+    }
 }
+
+
