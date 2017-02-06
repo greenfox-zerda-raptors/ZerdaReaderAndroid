5a925304f46f0624926271b1596c46338cd3aab0
Anna812 <88menyus@gmail.com>
2017-02-06 15:34:10 +0100

    create SignupActivity

---
+++ app/src/main/java/com/greenfox/fuchsit/zerdareader/activity/SignupActivity.java
@@ -0,0 +1,21 @@
+package com.greenfox.fuchsit.zerdareader.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.greenfox.fuchsit.zerdareader.R;
+
+/**
+ * Created by Anna on 17/02/06.
+ */
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.login);
+
+    }
+}
