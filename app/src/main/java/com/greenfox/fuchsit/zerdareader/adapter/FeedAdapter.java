f64e66b2a750c35098a689fdcd6c17cd8ba3af6b
regnisalram <reka.bence@gmail.com>
2017-02-06 14:24:02 +0100

    newsItem design done

--- app/src/main/java/com/greenfox/fuchsit/zerdareader/adapter/FeedAdapter.java
+++ app/src/main/java/com/greenfox/fuchsit/zerdareader/adapter/FeedAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.greenfox.fuchsit.zerdareader.R;
@@ -30,10 +31,12 @@ public class FeedAdapter extends ArrayAdapter<NewsItem> {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.newsitem, parent, false);
         }
         // Lookup view for data population
-        TextView titleTW = (TextView) convertView.findViewById(R.id.title);
+        ImageView email = (ImageView) convertView.findViewById(R.id.thumbnail);
+        TextView title = (TextView) convertView.findViewById(R.id.title);
 
         // Populate the data into the template view using the data object
-        titleTW.setText(newsItem.getTitle());
+        email.setImageResource(R.drawable.email);
+        title.setText(newsItem.getTitle());
 
         // Return the completed view to render on screen
         return convertView;
