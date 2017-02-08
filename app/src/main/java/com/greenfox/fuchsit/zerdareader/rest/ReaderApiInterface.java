b94eef614385b96c47fae9299e0dd12df84c7365
regnisalram <reka.bence@gmail.com>
2017-02-08 15:15:07 +0100

    retrofit extended

--- app/src/main/java/com/greenfox/fuchsit/zerdareader/rest/ReaderApiInterface.java
+++ app/src/main/java/com/greenfox/fuchsit/zerdareader/rest/ReaderApiInterface.java
@@ -1,5 +1,9 @@
 package com.greenfox.fuchsit.zerdareader.rest;
 
+import android.content.SharedPreferences;
+
+import com.greenfox.fuchsit.zerdareader.model.FavoriteRequest;
+import com.greenfox.fuchsit.zerdareader.model.FavoriteResponse;
 import com.greenfox.fuchsit.zerdareader.model.LoginRequest;
 import com.greenfox.fuchsit.zerdareader.model.NewsItem;
 import com.greenfox.fuchsit.zerdareader.model.UpdateRequest;
@@ -8,10 +12,12 @@ import com.greenfox.fuchsit.zerdareader.model.UserResponse;
 import java.util.ArrayList;
 
 import retrofit2.Call;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 /**
  * Created by Zsuzska on 2017. 01. 19..
@@ -19,11 +25,18 @@ import retrofit2.http.Path;
 
 public interface ReaderApiInterface {
 
-    @GET("/favorites")
-    Call<ArrayList<NewsItem>> getFavouriteNewsItems();
-
     @GET("/feed")
     Call<ArrayList<NewsItem>> getNewsItems();
+
+    //favourite cc anna
+    @GET("/favorites")
+    Call<ArrayList<NewsItem>> getFavouriteNewsItems(@Query("token") String token);
+
+    @POST("/favorites")
+    Call<FavoriteResponse> createFavoriteItem(@Query("token") String token, FavoriteRequest favoriteRequest);
+
+    @DELETE("/favorites")
+    Call<FavoriteResponse> deleteFavoriteItem(@Query("token") String token, FavoriteRequest favoriteRequest);
 
     @POST("user/login")
     Call<UserResponse> loginUser(LoginRequest loginRequest);
@@ -33,6 +46,8 @@ public interface ReaderApiInterface {
 
     @POST("user/signup")
     Call<UserResponse> signUpUser(LoginRequest loginRequest);
+
+
 
 }
 
