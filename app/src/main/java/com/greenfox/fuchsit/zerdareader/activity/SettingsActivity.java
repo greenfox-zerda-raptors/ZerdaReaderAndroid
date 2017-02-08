1045f2627fa4f195b8384704439afb35c6860545
zsofinnemeth <zsofinnemeth@gmail.com>
2017-02-08 16:36:08 +0100

    fragment dropped, I switched to a single settings activity

--- app/src/main/java/com/greenfox/fuchsit/zerdareader/activity/SettingsActivity.java
+++ app/src/main/java/com/greenfox/fuchsit/zerdareader/activity/SettingsActivity.java
@@ -1,48 +1,17 @@
 package com.greenfox.fuchsit.zerdareader.activity;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
+import android.preference.PreferenceActivity;
 
 import com.greenfox.fuchsit.zerdareader.R;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends PreferenceActivity {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
-        this.getFragmentManager().beginTransaction()
-                .replace(R.id.activity_settings, new SettingsFragment())
-                .commit();
-
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
-        setSupportActionBar((Toolbar) findViewById(R.id.my_toolbar));
-        myToolbar.setTitle("Feed");
-        myToolbar.setSubtitle("Back to your feed");
-        setSupportActionBar(myToolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    }
-
-    public boolean onCreateOptionsMenu(Menu menu) {
-
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.short_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                finish();
-                break;
-        }
-        return true;
+        addPreferencesFromResource(R.xml.preferences);
     }
 
 }
