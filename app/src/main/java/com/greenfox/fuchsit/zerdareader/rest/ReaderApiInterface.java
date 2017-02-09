0df36bcfcf7f58a1583662012362e6fce3265322
Anna812 <88menyus@gmail.com>
2017-02-09 13:05:56 +0100

    add @Query token to /favorites endpoint in ReaderApiInterface

--- app/src/main/java/com/greenfox/fuchsit/zerdareader/rest/ReaderApiInterface.java
+++ app/src/main/java/com/greenfox/fuchsit/zerdareader/rest/ReaderApiInterface.java
@@ -12,6 +12,7 @@ import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 /**
  * Created by Zsuzska on 2017. 01. 19..
@@ -20,7 +21,7 @@ import retrofit2.http.Path;
 public interface ReaderApiInterface {
 
     @GET("/favorites")
-    Call<ArrayList<NewsItem>> getFavouriteNewsItems();
+    Call<ArrayList<NewsItem>> getFavouriteNewsItems(@Query("token") String token);
 
     @GET("/feed")
     Call<ArrayList<NewsItem>> getNewsItems();
