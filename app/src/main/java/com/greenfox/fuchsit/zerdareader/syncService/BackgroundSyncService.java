bbd4459a041bb4440740ff6e39466fd0f5360363
Anna812 <88menyus@gmail.com>
2017-02-09 14:30:32 +0100

    extend BackgroundSyncService from IntentService, implement methods, create constructor and define fields

--- app/src/main/java/com/greenfox/fuchsit/zerdareader/syncService/BackgroundSyncService.java
+++ app/src/main/java/com/greenfox/fuchsit/zerdareader/syncService/BackgroundSyncService.java
@@ -1,8 +1,25 @@
 package com.greenfox.fuchsit.zerdareader.syncService;
 
+import android.app.IntentService;
+import android.content.Intent;
+import android.util.Log;
+
 /**
  * Created by Anna on 17/02/09.
  */
 
-public class BackgroundSyncService {
+public class BackgroundSyncService extends IntentService {
+    public static final String TRANSACTION_DONE = "com.geenfox.fuchsit.zerdareader.TRASACTION_DONE";
+
+    public BackgroundSyncService() {
+        super(BackgroundSyncService.class.getName());
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Log.e("BackgroundSyncService", "Service started");
+        String passedURL =  intent.getStringExtra("url");
+        updateNewsItems(passedURL);
+        Log.e("BackgroundSyncService", "Service stopped");
+    }
 }
