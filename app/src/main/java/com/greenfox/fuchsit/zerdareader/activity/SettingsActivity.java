b2ad2b7ecc6edb76c85ae72fc6862c063a379b38
zsofinnemeth <zsofinnemeth@gmail.com>
2017-02-09 11:14:44 +0100

    trying to display a toolbar

--- app/src/main/java/com/greenfox/fuchsit/zerdareader/activity/SettingsActivity.java
+++ app/src/main/java/com/greenfox/fuchsit/zerdareader/activity/SettingsActivity.java
@@ -11,6 +11,7 @@ public class SettingsActivity extends PreferenceActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
         addPreferencesFromResource(R.xml.preferences);
     }
 
