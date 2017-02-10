74e6b270af3aa8076069d03748abda550332577e
zsofinnemeth <zsofinnemeth@gmail.com>
2017-02-10 09:56:20 +0100

    settings displayed, no toolbar

--- app/src/main/java/com/greenfox/fuchsit/zerdareader/activity/SettingsActivity.java
+++ app/src/main/java/com/greenfox/fuchsit/zerdareader/activity/SettingsActivity.java
@@ -1,46 +1,31 @@
 package com.greenfox.fuchsit.zerdareader.activity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatCallback;
-import android.support.v7.app.AppCompatDelegate;
-import android.support.v7.view.ActionMode;
-import android.support.v7.widget.Toolbar;
 
 import com.greenfox.fuchsit.zerdareader.R;
 
-public class SettingsActivity extends PreferenceActivity implements AppCompatCallback {
+public class SettingsActivity extends PreferenceActivity {
 
-    private AppCompatDelegate delegate;
+    SharedPreferences sharedpreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.preferences);
-        delegate = AppCompatDelegate.create(this, this);
-        // delegate.setContentView(R.layout.activity_settings);
-        Toolbar myToolbar= (Toolbar) findViewById(R.id.my_toolbar);
-        delegate.setSupportActionBar(myToolbar);
+       super.onCreate(savedInstanceState);
+       addPreferencesFromResource(R.xml.preferences);
 
     }
 
-    @Override
-    public void onSupportActionModeStarted(ActionMode mode) {
+    public void enableBackgroundSync() {
 
     }
 
-    @Override
-    public void onSupportActionModeFinished(ActionMode mode) {
+    public void enablePushNotifications() {
 
     }
 
-    @Nullable
-    @Override
-    public ActionMode onWindowStartingSupportActionMode(ActionMode.Callback callback) {
-        return null;
-    }
 }
 
 
