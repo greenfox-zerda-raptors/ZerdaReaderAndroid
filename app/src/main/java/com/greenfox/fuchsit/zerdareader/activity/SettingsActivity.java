80845f5bdc3980fd6d7ed1654448adfc18e47e99
zsofinnemeth <zsofinnemeth@gmail.com>
2017-02-10 13:54:21 +0100

    functioning settings activity, missing toolbar

--- app/src/main/java/com/greenfox/fuchsit/zerdareader/activity/SettingsActivity.java
+++ app/src/main/java/com/greenfox/fuchsit/zerdareader/activity/SettingsActivity.java
@@ -2,15 +2,13 @@ package com.greenfox.fuchsit.zerdareader.activity;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.CheckBoxPreference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
 
 import com.greenfox.fuchsit.zerdareader.R;
 
-public class SettingsActivity extends PreferenceActivity implements SharedPreferences.OnSharedPreferenceChangeListener{
-
-    private CheckBoxPreference checkbox_sync, checkbox_push;
+public class SettingsActivity extends PreferenceActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
+    
     private SharedPreferences loginData;
 
 
@@ -30,3 +28,4 @@ public class SettingsActivity extends PreferenceActivity implements SharedPrefer
         addPreferencesFromResource(R.xml.preferences);
     }
 }
+
