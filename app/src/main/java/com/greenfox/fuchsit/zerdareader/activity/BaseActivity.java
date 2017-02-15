a56ef1ec5b96aae06e204d9f26e2a97f47cc12e5
zsofinnemeth <zsofinnemeth@gmail.com>
2017-02-15 14:47:12 +0100

    placing the showNewsItems method to Baseactivity

--- app/src/main/java/com/greenfox/fuchsit/zerdareader/activity/BaseActivity.java
+++ app/src/main/java/com/greenfox/fuchsit/zerdareader/activity/BaseActivity.java
@@ -6,10 +6,26 @@ import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 
 import com.greenfox.fuchsit.zerdareader.R;
+import com.greenfox.fuchsit.zerdareader.adapter.FeedAdapter;
+import com.greenfox.fuchsit.zerdareader.model.NewsItem;
+import com.greenfox.fuchsit.zerdareader.rest.ReaderApiInterface;
+
+import java.util.ArrayList;
+
+import javax.inject.Inject;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public abstract class BaseActivity extends AppCompatActivity {
 
-    SharedPreferences timePreferences;
+    SharedPreferences sharedPreferences;
+    FeedAdapter adapter;
+    int tabNumber;
+
+    @Inject
+    ReaderApiInterface apiService;
 
 
     @Override
@@ -23,10 +39,37 @@ public abstract class BaseActivity extends AppCompatActivity {
         super.onPause();
 
         Long timestamp = System.currentTimeMillis()/1000;
-        timePreferences = PreferenceManager.getDefaultSharedPreferences(BaseActivity.this);
-        final SharedPreferences.Editor editor = timePreferences.edit();
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(BaseActivity.this);
+        final SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.putLong("timestamp", timestamp);
     }
+
+
+    public void showNewsItems() {
+
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+
+        Call<ArrayList<NewsItem>> call;
+
+        if(tabNumber == 1) {
+            call = apiService.getNewsItems(sharedPreferences.getString("token", "default"));
+        } else {
+            call = apiService.getFavouriteNewsItems(sharedPreferences.getString("token", "default"));
+        }
+
+        call.enqueue(new Callback<ArrayList<NewsItem>>() {
+            @Override
+            public void onResponse(Call<ArrayList<NewsItem>> call, Response<ArrayList<NewsItem>> response) {
+                adapter.addAll(response.body());
+            }
+
+            @Override
+            public void onFailure(Call<ArrayList<NewsItem>> call, Throwable t) {
+
+            }
+        });
+    }
+
 
 }
 
