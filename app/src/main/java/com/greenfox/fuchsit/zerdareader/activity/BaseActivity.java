8906c5c137f36ef14848c11c9792d9c22f0edf34
zsofinnemeth <zsofinnemeth@gmail.com>
2017-02-15 11:01:11 +0100

    timestamp added + onPause method to save in sharedpreferences

--- app/src/main/java/com/greenfox/fuchsit/zerdareader/activity/BaseActivity.java
+++ app/src/main/java/com/greenfox/fuchsit/zerdareader/activity/BaseActivity.java
@@ -1,15 +1,29 @@
 package com.greenfox.fuchsit.zerdareader.activity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 
 import com.greenfox.fuchsit.zerdareader.R;
 
-public class BaseActivity extends AppCompatActivity {
+public abstract class BaseActivity extends AppCompatActivity {
+
+    SharedPreferences timePreferences;
+    Long timestamp = System.currentTimeMillis()/1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_base);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        timePreferences = PreferenceManager.getDefaultSharedPreferences(BaseActivity.this);
+        final SharedPreferences.Editor editor = timePreferences.edit();
+        editor.putLong("timestamp", timestamp);
     }
 }
