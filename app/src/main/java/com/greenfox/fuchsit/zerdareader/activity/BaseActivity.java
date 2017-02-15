933454ede42789fb04df674ce01192f45b96c04b
zsofinnemeth <zsofinnemeth@gmail.com>
2017-02-15 15:13:15 +0100

    showNewsItem cut short

--- app/src/main/java/com/greenfox/fuchsit/zerdareader/activity/BaseActivity.java
+++ app/src/main/java/com/greenfox/fuchsit/zerdareader/activity/BaseActivity.java
@@ -8,25 +8,15 @@ import android.support.v7.app.AppCompatActivity;
 import com.greenfox.fuchsit.zerdareader.R;
 import com.greenfox.fuchsit.zerdareader.adapter.FeedAdapter;
 import com.greenfox.fuchsit.zerdareader.model.NewsItem;
-import com.greenfox.fuchsit.zerdareader.rest.ReaderApiInterface;
 
 import java.util.ArrayList;
 
-import javax.inject.Inject;
-
-import retrofit2.Call;
-import retrofit2.Callback;
 import retrofit2.Response;
 
 public abstract class BaseActivity extends AppCompatActivity {
 
     SharedPreferences sharedPreferences;
     FeedAdapter adapter;
-    int tabNumber;
-
-    @Inject
-    ReaderApiInterface apiService;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,30 +34,9 @@ public abstract class BaseActivity extends AppCompatActivity {
         editor.putLong("timestamp", timestamp);
     }
 
-
-    public void showNewsItems() {
-
-        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
-
-        Call<ArrayList<NewsItem>> call;
-
-        if(tabNumber == 1) {
-            call = apiService.getNewsItems(sharedPreferences.getString("token", "default"));
-        } else {
-            call = apiService.getFavouriteNewsItems(sharedPreferences.getString("token", "default"));
-        }
-
-        call.enqueue(new Callback<ArrayList<NewsItem>>() {
-            @Override
-            public void onResponse(Call<ArrayList<NewsItem>> call, Response<ArrayList<NewsItem>> response) {
-                adapter.addAll(response.body());
-            }
-
-            @Override
-            public void onFailure(Call<ArrayList<NewsItem>> call, Throwable t) {
-
-            }
-        });
+    public void showNewsItems(Response<ArrayList<NewsItem>> response) {
+        adapter.clear();
+        adapter.addAll(response.body());
     }
 
 
