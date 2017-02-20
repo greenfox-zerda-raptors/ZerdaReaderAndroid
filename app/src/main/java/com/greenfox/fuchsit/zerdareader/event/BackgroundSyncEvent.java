f22f05d0668f117ae6e325510d8e676524ada60e
Anna812 <88menyus@gmail.com>
2017-02-20 13:40:23 +0100

    create BackgroundSyncEvent class

---
+++ app/src/main/java/com/greenfox/fuchsit/zerdareader/event/BackgroundSyncEvent.java
@@ -0,0 +1,14 @@
+package com.greenfox.fuchsit.zerdareader.event;
+
+/**
+ * Created by Anna on 17/02/20.
+ */
+
+public class BackgroundSyncEvent {
+
+    String status;
+
+    public BackgroundSyncEvent(String status) {
+        this.status = status;
+    }
+}
