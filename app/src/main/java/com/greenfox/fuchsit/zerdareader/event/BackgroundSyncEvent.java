0e9e6cb6b67d4fe11c67cdb7c6561e7a268a59e0
Anna812 <88menyus@gmail.com>
2017-02-20 13:57:06 +0100

    change BackgroundSyncEvent field to Arraylist of Newsitems, update getter too

--- app/src/main/java/com/greenfox/fuchsit/zerdareader/event/BackgroundSyncEvent.java
+++ app/src/main/java/com/greenfox/fuchsit/zerdareader/event/BackgroundSyncEvent.java
@@ -1,14 +1,22 @@
 package com.greenfox.fuchsit.zerdareader.event;
 
+import com.greenfox.fuchsit.zerdareader.model.NewsItem;
+
+import java.util.ArrayList;
+
 /**
  * Created by Anna on 17/02/20.
  */
 
 public class BackgroundSyncEvent {
 
-    String status;
+    ArrayList<NewsItem> newsList;
 
-    public BackgroundSyncEvent(String status) {
-        this.status = status;
+    public BackgroundSyncEvent(ArrayList<NewsItem> newsList) {
+        this.newsList = newsList;
+    }
+
+    public Object getNewsList() {
+        return newsList;
     }
 }
