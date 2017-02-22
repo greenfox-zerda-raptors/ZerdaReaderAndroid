b16f10bd39605cbc7dad66af6cc4b3022059dd47
regnisalram <reka.bence@gmail.com>
2017-02-22 21:52:45 +0100

    Body added to endpoints, updateOpened changed to have response

--- app/src/main/java/com/greenfox/fuchsit/zerdareader/rest/ReaderApiInterface.java
+++ app/src/main/java/com/greenfox/fuchsit/zerdareader/rest/ReaderApiInterface.java
@@ -2,16 +2,15 @@ package com.greenfox.fuchsit.zerdareader.rest;
 
 import com.greenfox.fuchsit.zerdareader.model.FavoriteRequest;
 import com.greenfox.fuchsit.zerdareader.model.FavoriteResponse;
+import com.greenfox.fuchsit.zerdareader.model.FeedResponse;
 import com.greenfox.fuchsit.zerdareader.model.LoginRequest;
-import com.greenfox.fuchsit.zerdareader.model.NewsItem;
 import com.greenfox.fuchsit.zerdareader.model.UpdateRequest;
 import com.greenfox.fuchsit.zerdareader.model.UserResponse;
 
-import java.util.ArrayList;
-
 import retrofit2.Call;
-import retrofit2.http.DELETE;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.HTTP;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
 import retrofit2.http.Path;
@@ -24,27 +23,26 @@ import retrofit2.http.Query;
 public interface ReaderApiInterface {
 
     @GET("/feed")
-    Call<ArrayList<NewsItem>> getNewsItems(@Query("token") String token);
+    Call<FeedResponse> getNewsItems(@Query("token") String token);
 
     //favourite cc anna
     @GET("/favorites")
-    Call<ArrayList<NewsItem>> getFavouriteNewsItems(@Query("token") String token);
+    Call<FeedResponse> getFavouriteNewsItems(@Query("token") String token);
 
     @POST("/favorites")
-    Call<FavoriteResponse> createFavoriteItem(@Query("token") String token, FavoriteRequest favoriteRequest);
+    Call<FavoriteResponse> createFavoriteItem(@Query("token") String token, @Body FavoriteRequest favoriteRequest);
 
-    @DELETE("/favorites")
-    Call<FavoriteResponse> deleteFavoriteItem(@Query("token") String token, FavoriteRequest favoriteRequest);
+    @HTTP(method = "DELETE", path = "/favorites", hasBody = true)
+    Call<FavoriteResponse> deleteFavoriteItem(@Query("token") String token, @Body FavoriteRequest favoriteRequest);
 
     @POST("user/login")
-    Call<UserResponse> loginUser(LoginRequest loginRequest);
+    Call<UserResponse> loginUser(@Body LoginRequest loginRequest);
 
     @PUT("/feed/{item_id}")
-    void updateOpened(@Path("item_id") long id, @Query("token") String token, UpdateRequest updateRequest);
+    Call<okhttp3.ResponseBody> updateOpened(@Path("item_id") long id, @Query("token") String token, @Body UpdateRequest updateRequest);
 
     @POST("user/signup")
-    Call<UserResponse> signUpUser(LoginRequest loginRequest);
-
+    Call<UserResponse> signUpUser(@Body LoginRequest loginRequest);
 
 
 }
