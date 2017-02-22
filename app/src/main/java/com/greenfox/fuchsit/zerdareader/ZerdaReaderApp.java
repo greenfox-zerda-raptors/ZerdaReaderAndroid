ee729ecaf8807855066f70d8a8f105844e72976f
Anna812 <88menyus@gmail.com>
2017-02-22 23:05:11 +0100

    implement Application.ActivityLifecycleCallbacks in ZerdaReaderApp and add public static boolean field

--- app/src/main/java/com/greenfox/fuchsit/zerdareader/ZerdaReaderApp.java
+++ app/src/main/java/com/greenfox/fuchsit/zerdareader/ZerdaReaderApp.java
@@ -1,14 +1,21 @@
 package com.greenfox.fuchsit.zerdareader;
 
+import android.app.Activity;
 import android.app.Application;
+import android.os.Bundle;
 
 /**
  * Created by regnisalram on 2/16/17.
  */
 
-public class ZerdaReaderApp extends Application{
+public class ZerdaReaderApp extends Application implements Application.ActivityLifecycleCallbacks{
 
     static Application application;
+    public static boolean inForeground;
+
+    public static Application getApplication() {
+        return application;
+    }
 
     @Override
     public void onCreate() {
@@ -16,7 +23,38 @@ public class ZerdaReaderApp extends Application{
         application = this;
     }
 
-    public static Application getApplication() {
-        return application;
+    @Override
+    public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+        inForeground = true;
+    }
+
+    @Override
+    public void onActivityStarted(Activity activity) {
+        inForeground = true;
+    }
+
+    @Override
+    public void onActivityResumed(Activity activity) {
+        inForeground = true;
+    }
+
+    @Override
+    public void onActivityPaused(Activity activity) {
+        inForeground = false;
+    }
+
+    @Override
+    public void onActivityStopped(Activity activity) {
+        inForeground = false;
+    }
+
+    @Override
+    public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+
+    }
+
+    @Override
+    public void onActivityDestroyed(Activity activity) {
+        inForeground = false;
     }
 }
