f3d7486dd83457455496c2357e2b749e28bcf13a
Anna812 <88menyus@gmail.com>
2017-02-23 10:53:24 +0100

    revert finally, please

--- app/src/main/java/com/greenfox/fuchsit/zerdareader/ZerdaReaderApp.java
+++ app/src/main/java/com/greenfox/fuchsit/zerdareader/ZerdaReaderApp.java
@@ -1,29 +1,14 @@
 package com.greenfox.fuchsit.zerdareader;
 
-import android.app.Activity;
 import android.app.Application;
-import android.os.Bundle;
 
 /**
  * Created by regnisalram on 2/16/17.
  */
 
-public class ZerdaReaderApp extends Application implements Application.ActivityLifecycleCallbacks{
+public class ZerdaReaderApp extends Application{
 
     static Application application;
-    public static boolean inForeground;
-    private static ZerdaReaderApp zerdaReaderApp;
-
-    public static void init(Application app){
-        if (zerdaReaderApp == null){
-            zerdaReaderApp = new ZerdaReaderApp();
-            app.registerActivityLifecycleCallbacks(zerdaReaderApp);
-        }
-    }
-
-    public static Application getApplication() {
-        return application;
-    }
 
     @Override
     public void onCreate() {
@@ -31,37 +16,7 @@ public class ZerdaReaderApp extends Application implements Application.ActivityL
         application = this;
     }
 
-    @Override
-    public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
-        inForeground = true;
-    }
-
-    @Override
-    public void onActivityStarted(Activity activity) {
-        inForeground = true;
-    }
-
-    @Override
-    public void onActivityResumed(Activity activity) {
-        inForeground = true;
-    }
-
-    @Override
-    public void onActivityPaused(Activity activity) {
-        inForeground = false;
-    }
-
-    @Override
-    public void onActivityStopped(Activity activity) {
-        inForeground = false;
-    }
-
-    @Override
-    public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
-    }
-
-    @Override
-    public void onActivityDestroyed(Activity activity) {
-        inForeground = false;
+    public static Application getApplication() {
+        return application;
     }
 }
