ad23840ef56c0d15e1e6618005bb67487b96ac76
Anna812 <88menyus@gmail.com>
2017-02-23 13:17:11 +0100

    move backgroundsync related stuff from SettingsActivity to ZerdaReaderApp

--- app/src/main/java/com/greenfox/fuchsit/zerdareader/ZerdaReaderApp.java
+++ app/src/main/java/com/greenfox/fuchsit/zerdareader/ZerdaReaderApp.java
@@ -1,6 +1,18 @@
 package com.greenfox.fuchsit.zerdareader;
 
+import android.app.AlarmManager;
 import android.app.Application;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import com.greenfox.fuchsit.zerdareader.backgroundSync.BackgroundSyncReceiver;
+import com.greenfox.fuchsit.zerdareader.event.BackSyncSettingEvent;
+import com.greenfox.fuchsit.zerdareader.event.LeavingApplicationEvent;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
 
 /**
  * Created by regnisalram on 2/16/17.
@@ -9,14 +21,71 @@ import android.app.Application;
 public class ZerdaReaderApp extends Application{
 
     static Application application;
+    public static boolean visible;
+    public static boolean startingActivity;
+    private AlarmManager alarm;
+
+    public static Application getApplication() {
+        return application;
+    }
 
     @Override
     public void onCreate() {
         super.onCreate();
         application = this;
+        EventBus.getDefault().register(this);
     }
 
-    public static Application getApplication() {
-        return application;
+    @Override
+    public void onTerminate() {
+        EventBus.getDefault().unregister(this);
+        super.onTerminate();
+    }
+
+    @Subscribe
+    public void onLeavingApplicationEvent(LeavingApplicationEvent event) {
+        PendingIntent pIntent = setupPendingIntent();
+        alarm.setInexactRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), defineInterval(), pIntent);
+    }
+
+    @Subscribe
+    public void onBackSyncSettingEvent(BackSyncSettingEvent event) {
+        enableBackgroundSync(event.enabled);
+    }
+
+    public void enableBackgroundSync(boolean checkboxChecked) {
+        if(checkboxChecked) {
+            Log.e("SettingsActivity", "background sync enabled");
+            scheduleAlarm();
+        } else {
+            Log.e("SettingsActivity", "background sync disabled");
+            cancelAlarm();
+        }
+    }
+
+    public void scheduleAlarm() {
+        final PendingIntent pIntent = setupPendingIntent();
+        alarm = (AlarmManager) this.getSystemService(Context.ALARM_SERVICE);
+        alarm.setInexactRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), defineInterval(), pIntent);
+    }
+
+    public PendingIntent setupPendingIntent() {
+        Intent intent = new Intent(this, BackgroundSyncReceiver.class);
+        return PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+    }
+
+    private long defineInterval() {
+        if(visible) {
+            Log.e("App", "in Foreground");
+            return 120000L;
+        } else {
+            Log.e("App", "not in Foreground");
+            return 600000L;
+        }
+    }
+
+    public void cancelAlarm() {
+        alarm = (AlarmManager) this.getSystemService(Context.ALARM_SERVICE);
+        alarm.cancel(setupPendingIntent());
     }
 }
--- app/src/main/java/com/greenfox/fuchsit/zerdareader/activity/SettingsActivity.java
+++ app/src/main/java/com/greenfox/fuchsit/zerdareader/activity/SettingsActivity.java
@@ -1,20 +1,15 @@
 package com.greenfox.fuchsit.zerdareader.activity;
 
-import android.app.AlarmManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
-import android.util.Log;
 
 import com.greenfox.fuchsit.zerdareader.R;
 import com.greenfox.fuchsit.zerdareader.ZerdaReaderApp;
-import com.greenfox.fuchsit.zerdareader.backgroundSync.BackgroundSyncReceiver;
+import com.greenfox.fuchsit.zerdareader.event.BackSyncSettingEvent;
 
-import java.util.Set;
+import org.greenrobot.eventbus.EventBus;
 
 public class SettingsActivity extends PreferenceActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
     
@@ -34,42 +29,12 @@ public class SettingsActivity extends PreferenceActivity implements SharedPrefer
     public void onSharedPreferenceChanged(SharedPreferences loginData, String key) {
         setPreferenceScreen(null);
         addPreferencesFromResource(R.xml.preferences);
-        enableBackgroundSync(preferences.getBoolean("checkbox_sync", true));
+        EventBus.getDefault().post(new BackSyncSettingEvent(preferences.getBoolean("checkbox_sync", true)));
     }
 
-    public void enableBackgroundSync(boolean checkboxChecked) {
-        if(checkboxChecked) {
-            Log.e("SettingsActivity", "background sync enabled");
-            scheduleAlarm();
-        } else {
-            Log.e("SettingsActivity", "background sync disabled");
-            cancelAlarm();
-        }
-    }
-
-    public void scheduleAlarm() {
-        final PendingIntent pIntent = setupPendingIntent();
-        AlarmManager alarm = (AlarmManager) this.getSystemService(Context.ALARM_SERVICE);
-        alarm.setInexactRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), defineInterval(), pIntent);
-    }
-
-    private long defineInterval() {
-        if(ZerdaReaderApp.inForeground) {
-            Log.e("SettingsAct", "in Foreground");
-            return 120000L;
-        } else {
-            Log.e("SettingsAct", "not in Foreground");
-            return 600000L;
-        }
-    }
-
-    public void cancelAlarm() {
-        AlarmManager alarm = (AlarmManager) this.getSystemService(Context.ALARM_SERVICE);
-        alarm.cancel(setupPendingIntent());
-    }
-
-    public PendingIntent setupPendingIntent() {
-        Intent intent = new Intent(this, BackgroundSyncReceiver.class);
-        return PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+    @Override
+    protected void onResume() {
+        super.onResume();
+        ZerdaReaderApp.visible = true;
     }
 }
